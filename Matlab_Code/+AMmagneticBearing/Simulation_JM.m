4b22c46626c77af6ef8a558aca98fd27004c9dde
Georg Balke <georg.balke@tum.de>
2017-10-26 10:36:17 +0200

    no message

--- Matlab_Code/+AMmagneticBearing/Simulation_JM.m
+++ Matlab_Code/+AMmagneticBearing/Simulation_JM.m
@@ -1,15 +1,19 @@
 %% Simulation Magnetlager_ANTON
-
-
+% Geometrie nicht bearbeitbar. Mögliche Lösungen:
+% dxf importieren
+% Stl bearbeiten
+% stl's addieren
+% diskret+analytisch kombinieren
+% aus dem Mesh zurück
+% programm mit parfor beschleunigen??
 %% Aufräumen
-
 clear all;
 close all;
 clc;
 
 %% Importieren der Bibliothek
 
-import AMB_Sim.*
+import AMmagneticBearing.2D_Matlab.*
 
 %% Berechnen einer Kraft
 
@@ -17,26 +21,31 @@ Configuration_ML_ANTON
 
 ML=MagneticBearing('Anton',cnfg);
 
-ML.init();
+ML.init(); %cnfg --> model
 
-%ML.generate_geometry(cnfg.geometry);
+%ML.generate_geometry(cnfg.geometry); %aus den punkten in cnfg werden kanten generiert, wird eine geometrie errechnet
+%HIER ist die Stelle zum Importieren   
+%importGeometry(ML.model,'Schnitt.stl');
 %ML.show_geometry();
-%ML.show_mesh();
-%ML.show_solution(result);
 
-%[result,W] = ML.calculate_energy([0,0.001],[0,2],[0,1]);
+[result,W] = ML.calculate_energy([0.02,0.001],[0,2],[0,1]);% in calculate_energy werden den
+  
+ML.show_solution(result);
+% spulen die faces zugeordnet!! 
 
 %[Fx,Fy]=ML.calculate_force([0,0],[0,1],[0,2],1e-9);
 
 
 %% Berechnen der Kennfelder
 
-charmap_lin = gen_lin_map(ML,[-0.0005:0.0001:0.0005],[-2:0.5:2],[0],[0],2,1e-5);
-
-save daten_Kennfeld.mat charmap_lin
-
-%charmap_nonlin = gen_nonlin_map([-0.0003:0.0001:0.0003],[-2:1:2],[0],[0],1,1e-9);
-
-%% Grafiken erstellen
-Kp = Graphics.CharacteristicMap('Kennlinie linear',charmap_lin);
-Kp.plot
+% %charmap_lin = gen_lin_map(ML,[-0.0005:0.0001:0.0005],[-2:0.5:2],[0],[0],2,1e-5);
+%charmap_lin = gen_lin_map(ML,[-0.0005:0.0005:0.0005],[-1:0.5:1],[0],[0],2,1e-3);
+% Anpassungen: calculate_energy: self.generate_geoetry wegkommentiert
+% 
+% save daten_Kennfeld.mat charmap_lin
+% 
+% %charmap_nonlin = gen_nonlin_map([-0.0003:0.0001:0.0003],[-2:1:2],[0],[0],1,1e-9);
+% 
+% %% Grafiken erstellen
+% Kp = Graphics.CharacteristicMap('Kennlinie linear',charmap_lin);
+% Kp.plot
