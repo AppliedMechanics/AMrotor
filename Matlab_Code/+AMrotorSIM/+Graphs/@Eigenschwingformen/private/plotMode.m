a0b2cd1b509a05201e5d49bafd739e9e4f207645
Christoph Goering <christoph.goering@tum.de>
2017-10-27 16:14:21 +0200

    Eigenmoden mit groesster Amplitude immer nach oben

--- Matlab_Code/+AMrotorSIM/+Graphs/@Eigenschwingformen/private/plotMode.m
+++ Matlab_Code/+AMrotorSIM/+Graphs/@Eigenschwingformen/private/plotMode.m
@@ -2,6 +2,11 @@ function plotMode( axFigure, x, V, D )
 %PLOTMODE Summary of this function goes here
 %   Detailed explanation goes here
 
+% find the maximum absolute value
+[~,index] = max(abs(V),[],1);
+% possible change sign of V so biggest amplitude is positive
+V = sign(V(index))*V;
+% plot
 plot(axFigure,x,V/norm(V),...
             'DisplayName',sprintf('%1.2f Hz',D/(2*pi)))
 
