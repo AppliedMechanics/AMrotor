83505fbfa54fcba13ee3350a3876f0f670b28e64
Michael Kreutz <m.kreutz@tum.de>
2018-10-17 11:39:47 +0200

    live plot of beam deflection: only plot the last/current column

--- Matlab_Code/+AMrotorSIM/+Experiments/@Stationaere_Lsg/private/odeOutputFcn_plotBeam.m
+++ Matlab_Code/+AMrotorSIM/+Experiments/@Stationaere_Lsg/private/odeOutputFcn_plotBeam.m
@@ -10,6 +10,9 @@ switch flag
         status=0;
         close
     case ''
+%         plot(y(1:6:end/2,end)); % plot Auslenkung in x
+        plot(sqrt(y(1:6:end/2,end).^2+y(2:6:end/2,end).^2)); %plot den Betrag der Auslenkung
+        ylim([-1 1]*5e-5);
         drawnow
         status=0;
 end
