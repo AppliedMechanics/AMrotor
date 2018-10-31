c00a82bd54d768ec56dace89dfe7e0f3c20172cc
Johannes Maierhofer <j.maierhofer@tum.de>
2018-10-31 15:40:05 +0100

    Refactoring - Function assemble in rotorsystem simplified

--- Matlab_Code/+AMrotorSIM/@Rotorsystem/Rotorsystem.m
+++ Matlab_Code/+AMrotorSIM/@Rotorsystem/Rotorsystem.m
@@ -32,34 +32,5 @@ classdef Rotorsystem < handle
        end
 
    end
-   %%
-   methods(Access=private)
-      % Rotor
-      function add_FEMRotor(obj,arg)
-         obj.rotor = AMrotorSIM.Rotor.FEMRotor.FeModel(arg); 
-      end
-      
-      function add_Rotor(obj,arg)
-          obj.rotor = AMrotorSIM.Rotor.Rotor(arg);
-      end
-      % Disc
-      function add_Disc(obj,arg)
-          obj.discs = AMrotorSIM.Disc(arg);
-      end
-      % Sensor
-      function add_Sensor(obj,arg)
-         obj.sensors(end+1) = AMrotorSIM.Sensors.(arg.type)(arg);
-      end
-      
-      % Lager
-      function add_Bearing(obj,arg)
-          obj.bearings(end+1) = AMrotorSIM.Bearings.(arg.type)(arg);
-      end
-      % Loads
-      function add_Load(obj,arg)
-          obj.loads(end+1) = AMrotorSIM.Loads.(arg.type)(arg);
-      end
-
-   end
    
 end
