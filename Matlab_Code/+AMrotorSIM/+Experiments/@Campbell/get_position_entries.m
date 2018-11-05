f9bec0d066d0aedcc06c421f62b48bc2ef029c97
Michael Kreutz <m.kreutz@tum.de>
2018-11-05 10:54:41 +0100

    Campbell-Berechnung so erweitert, dass 6 dof berücksichtigt werden damit sind auch die EW die mit Campbell berechnet wurden gleich zu den mit Modalanalyse berechneten

--- Matlab_Code/+AMrotorSIM/+Experiments/@Campbell/get_position_entries.m
+++ Matlab_Code/+AMrotorSIM/+Experiments/@Campbell/get_position_entries.m
@@ -1,5 +1,5 @@
 function Vpos = get_position_entries(obj,V)
     n.nodes = length(obj.rotorsystem.rotor.mesh.nodes);
-    ind = n.nodes*2*2+1:n.nodes*2*4;
+    ind = n.nodes*2*3+1:n.nodes*2*6;
     Vpos = V(ind,:);
 end
