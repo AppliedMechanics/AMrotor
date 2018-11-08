187f50fe7c22012119660023b7962d101b154b94
Michael Kreutz <m.kreutz@tum.de>
2018-11-08 13:50:58 +0100

    Campbell: back to 4 dof, because of problems with identification of forward and backward whirl of torsional modes

--- Matlab_Code/+AMrotorSIM/+Experiments/@Campbell/get_state_space_matrices.m
+++ Matlab_Code/+AMrotorSIM/+Experiments/@Campbell/get_state_space_matrices.m
@@ -1,19 +1,20 @@
 function [A,B] = get_state_space_matrices(obj,omega)
     n.nodes = length(obj.rotorsystem.rotor.mesh.nodes);
-    n.entries = n.nodes*6*2; % 6 because of 6 dof per node and 2 because
+    n.entries = n.nodes*4*2; % 4 because of 4 dof per node and 2 because
                              % matrices will be stated as state space
                              % system
     
     [M,C,G,K]= obj.rotorsystem.assemble_system_matrices(omega*60/2/pi);
                              
-%     ind_red = 1:n.nodes*6;
-%     ind_z = 3:6:n.nodes*6;
-%     ind_psi_z = 6:6:n.nodes*6;
-%     ind_red = setdiff(ind_red,[ind_z,ind_psi_z]); % remove dof u_z psi_z for compatibility, no information on torsional and axial eigenbehaviour 
-%     M = M(ind_red,ind_red);
-%     K = K(ind_red,ind_red);
-%     C = sparse(length(ind_red)); %neglect damping for better convergence
-%     G = G(ind_red,ind_red);
+    ind_red = 1:n.nodes*6;
+    ind_z = 3:6:n.nodes*6;
+    ind_psi_z = 6:6:n.nodes*6;
+    ind_red = setdiff(ind_red,[ind_z,ind_psi_z]); % remove dof u_z psi_z for compatibility, no information on torsional and axial eigenbehaviour 
+    M = M(ind_red,ind_red);
+    C = C(ind_red,ind_red);
+    G = G(ind_red,ind_red);
+    K = K(ind_red,ind_red);
+    
     A = sparse(n.entries,n.entries);
     B = sparse(n.entries,n.entries);
     
