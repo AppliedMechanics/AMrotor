f3f9870c38719e7eaae1812be4109765b9d0735a
Michael Kreutz <m.kreutz@tum.de>
2018-11-19 16:47:18 +0100

    LookUpTable Seal compatible with new data structure

--- Matlab_Code/+AMrotorSIM/+Seals/@LookUpTableSeal/LookUpTable.m
+++ Matlab_Code/+AMrotorSIM/+Seals/@LookUpTableSeal/LookUpTable.m
@@ -1,19 +1,38 @@
-function [ M_seal,D_seal ,K_seal ] = LookUpTable( self, rpm_current )     
+function [ M_seal, D_seal ,K_seal ] = LookUpTable( self, rpm_current )     
 
 Table = self.cnfg.sealModel.Table;
+K_seal = zeros(6,6);
+for i = [1,2,4,5]
+    for j = [1,2,4,5] % keine Vorgabe der Steifigkeit in z-Richtung
+        K_seal(i,j) = interp1(Table.rpm, Table.stiffness_matrix{i,j}, rpm_current, 'spline');
+    end
+end
+K_seal = sparse(K_seal);
+% dof-order: ux,uy,uz,psix,psiy,psiz
 
-md = interp1(Table.rpm, Table.m_xx, rpm_current, 'spine');
-Cd = interp1(Table.rpm, Table.d_xx, rpm_current, 'spline');
-cc = interp1(Table.rpm, Table.d_xy, rpm_current, 'spline');
-Kd = interp1(Table.rpm, Table.k_xx, rpm_current, 'spline');
-kc = interp1(Table.rpm, Table.k_xy, rpm_current, 'spline');
+D_seal = zeros(6,6);
+for i = [1,2,4,5]
+    for j = [1,2,4,5] % keine Vorgabe der Steifigkeit in z-Richtung
+        D_seal(i,j) = interp1(Table.rpm, Table.damping_matrix{i,j}, rpm_current, 'spline');
+    end
+end
+D_seal = sparse(D_seal);
+% dof-order: ux,uy,uz,psix,psiy,psiz
 
+M_seal = zeros(6,6);
+for i = [1,2,4,5]
+    for j = [1,2,4,5] % keine Vorgabe der Steifigkeit in z-Richtung
+        M_seal(i,j) = interp1(Table.rpm, Table.mass_matrix{i,j}, rpm_current, 'spline');
+    end
+end
+M_seal = sparse(M_seal);
+% dof-order: ux,uy,uz,psix,psiy,psiz
 
-M_seal = [md, 0; 0, md];
-D_seal = [Cd, -cc; cc, Cd];
-K_seal = [Kd, -kc; kc, Kd];
+self.stiffness_matrix = K_seal;
+self.damping_matrix = D_seal;
+self.damping_matrix = M_seal; 
 
-if rpm_current > 1*max(Table.rpm)
+if rpm_current > max(Table.rpm)
     warning(['Extrapolation of Seal-Coefficients from Look-Up-Table ',... 
             'at rotational speed of more than max(Table.rpm). ',...
             'Results may be inaccurate!']);
--- Matlab_Code/+AMrotorSIM/+Seals/@LookUpTableSeal/get_loc_system_matrices.m
+++ Matlab_Code/+AMrotorSIM/+Seals/@LookUpTableSeal/get_loc_system_matrices.m
@@ -1,18 +1,11 @@
 function [M_s,D_s,K_s] = get_loc_system_matrices(self,rpm)
 % dof-order: ux,uy,uz,psix,psiy,psiz
 
-M = sparse(6,6);
-D = sparse(6,6);
-K = sparse(6,6);
-
 [ M_s,D_s,K_s ] = self.LookUpTable( rpm );
 
-M(1:2,1:2) = M_s;
-D(1:2,1:2) = D_s;
-K(1:2,1:2) = K_s;
 
-self.mass_matrix = M;
-self.damping_matrix = D;
-self.stiffness_matrix = K;
+self.mass_matrix = M_s';
+self.damping_matrix = D_s';
+self.stiffness_matrix = K_s';
 
 end
