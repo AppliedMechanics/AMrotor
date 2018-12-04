a4e4cdd9a1f7c871d051fe0218b32574c92d3bc5
Michael Kreutz <m.kreutz@tum.de>
2018-12-04 10:24:30 +0100

    Unbalance_static - re-included contribution because of rotational acceleration

--- Matlab_Code/+AMrotorSIM/+Loads/@Unbalance_static/get_loc_load_vec.m
+++ Matlab_Code/+AMrotorSIM/+Loads/@Unbalance_static/get_loc_load_vec.m
@@ -1,19 +1,28 @@
 function get_loc_load_vec(obj,time,node,varargin)
 
-%Constant fix force 
+    %Constant fix force 
     obj.h = sparse(6,1);    
     % dof-order: ux,uy,uz,psix,psiy,psiz
 
     %%
     unwucht = obj.cnfg.betrag;
     phase = obj.cnfg.winkellage;
-    omega = node.omega;
-
-    obj.h(1) = unwucht * omega^2 * cos(omega*time + phase);
-    obj.h(2) = unwucht * omega^2 * sin(omega*time + phase);
+    omega = node.qd(6);
+    omegadot = node.qdd(6);
     
-    % Achtung: Hier wird der Anteil der Drehbeschleunigung vernachlässigt
-    % und nur der Anteil aus der Zentripetalkraft berücksichtigt. Die
-    % Drehbeschleunigung domega verursacht sowohl Kraefte als auch ein
-    % Drehmoment.
+    % Anteil aufgrund Zentripetalkraft
+    F_ZP(1) = unwucht * omega^2 * cos(omega*time + phase);
+    F_ZP(2) = unwucht * omega^2 * sin(omega*time + phase);
+    
+    % Anteil aufgrund der Drehbeschleunigung:
+    F_DB(1) = + unwucht * omegadot * sin(omega*time + phase);
+    F_DB(2) = - unwucht * omegadot * cos(omega*time + phase);
+    
+    % Zusammenstellen
+    obj.h(1) = F_ZP(1) + F_DB(1);
+    obj.h(2) = F_ZP(2) + F_DB(2);
+    
+    % evtl. weitere Schritte: Drehmoment aufgrund F_DB hinzufuegen
+    
+    
 end
