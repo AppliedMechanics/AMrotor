688e8bfb02ba9494b4ffdb7d0aeaee550eccca93
Michael Kreutz <m.kreutz@tum.de>
2018-12-04 10:25:12 +0100

    added seal (Childs-model) as load

--- Matlab_Code/+AMrotorSIM/+Loads/@ChildsSeal/get_loc_load_vec.m
+++ Matlab_Code/+AMrotorSIM/+Loads/@ChildsSeal/get_loc_load_vec.m
@@ -1,13 +1,14 @@
-function get_loc_load_vec(obj,~,omega,u_node)
+function get_loc_load_vec(self,~,node)
 
-u = u_node(1:2);
+u = node.q(1:2);
+ud = node.qd(1:2);
+udd = node.qdd(1:2);
+omega = node.qd(6);
 
 % dof-order: ux,uy,uz,psix,psiy,psiz
 
-rpm = omega/2/pi*60;
-
-if rpm == 0 %ChildsModel does not work for rpm=0 because of a division by rpm
-    rpm=1;
+if omega == 0 %ChildsModel does not work for rpm=0 because of a division by rpm
+    omega =0.1;
 end
 
 sys = self.cnfg.sealModel.sys;
@@ -21,12 +22,14 @@ init.omega0AR = [0;0;0];
 init.x0IR = [0;0.0;0];
 init.phi0IR = [0;0;0];
 init.xd0IR = [0;0;0];
-init.omega0IR = [rpm*pi/30;0;0];       % Einheit: rad/s !
+init.omega0IR = [omega;0;0];       % Einheit: rad/s !
 
-[ M_s,D_s,K_s ] = obj.ChildsModel( sys, init );
+[ M_s,D_s,K_s ] = self.ChildsModel( sys, init );
 
-F = M_s * udd + D_s * ud + K_s * u; % Problem: Wie bekomme ich die Beschleunigung des Knotens?
+F = -(M_s * udd + D_s * ud + K_s * u); % Damit bekomme ich eine unendliche Kraft -> keine Konvergenz
+%F = -(D_s * ud + K_s * u);
+%warning('Ohne Einfluss der Massenmatrix der Dichtung, da sonst keine Konvergenz.')
 
-obj.h = [F; zeros(4,1)];
+self.h = [F; zeros(4,1)];
 
 end
