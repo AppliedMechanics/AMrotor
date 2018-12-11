21aff9bc4673fab529ddefdcebf8d32b707ad609
Michael Kreutz <m.kreutz@tum.de>
2018-12-11 16:01:49 +0100

    Stastionaere Lsg: expanded state space to include accelerations

--- Matlab_Code/+AMrotorSIM/+Experiments/@Stationaere_Lsg/private/integrate_function_variant.m
+++ Matlab_Code/+AMrotorSIM/+Experiments/@Stationaere_Lsg/private/integrate_function_variant.m
@@ -4,15 +4,23 @@ A=mat.A;
 B=mat.B;
 ndof = length(A)/2;
 
+A=[A, zeros(2*ndof,ndof); zeros(ndof,2*ndof), eye(ndof)];
+B=[B, zeros(2*ndof,ndof); zeros(ndof,2*ndof), zeros(ndof)];
+
 Z(2*ndof)=Omega; % node on the right is driven with constant omega
 
-qdotdot = zeros(ndof,1);
+qdotdot = Z(ndof*2+1:end);
 
 
 %% Loadvector
 h_ges = rotorsystem.compute_system_load_variant(t,Z(1:2*ndof),qdotdot);
+h_ges = [h_ges; zeros(ndof,1)];
 
 %% DGL
 dZ = A*Z+B*h_ges; 
+
+% expanded state-space to maintain acceleration information for next integration step
+% dZ = [qdot, qdotdot, qdotdot], Z = [q, qdot, qdotdot]
+
 end
 
