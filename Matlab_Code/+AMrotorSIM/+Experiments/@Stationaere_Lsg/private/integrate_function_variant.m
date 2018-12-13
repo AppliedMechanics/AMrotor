578f4adeae2540c5e3be3e6adf2a38ce721498ee
Kreutz <ga54taf@mytum.de>
2018-12-13 12:23:50 +0100

    refactoring: clear naming of the expanded state vector easier to distinct between the "normal" (old) state vector and the expanded vector, that included the accelerations

--- Matlab_Code/+AMrotorSIM/+Experiments/@Stationaere_Lsg/private/integrate_function_variant.m
+++ Matlab_Code/+AMrotorSIM/+Experiments/@Stationaere_Lsg/private/integrate_function_variant.m
@@ -1,4 +1,4 @@
-function dZ = integrate_function_variant(t,Z,Omega, rotorsystem, mat)
+function dZ = integrate_function_variant(t,Zexpanded,Omega, rotorsystem, mat)
 
 A=mat.A;
 B=mat.B;
@@ -7,17 +7,17 @@ ndof = length(A)/2;
 A=[A, zeros(2*ndof,ndof); zeros(ndof,2*ndof), eye(ndof)];
 B=[B, zeros(2*ndof,ndof); zeros(ndof,2*ndof), zeros(ndof)];
 
-Z(2*ndof)=Omega; % node on the right is driven with constant omega
+Zexpanded(2*ndof)=Omega; % node on the right is driven with constant omega
 
-qdotdot = Z(ndof*2+1:end);
+qdotdot = Zexpanded(ndof*2+1:end);
 
 
 %% Loadvector
-h_ges = rotorsystem.compute_system_load_variant(t,Z(1:2*ndof),qdotdot);
+h_ges = rotorsystem.compute_system_load_variant(t,Zexpanded(1:2*ndof),qdotdot);
 h_ges = [h_ges; zeros(ndof,1)];
 
 %% DGL
-dZ = A*Z+B*h_ges; 
+dZ = A*Zexpanded+B*h_ges; 
 
 % expanded state-space to maintain acceleration information for next integration step
 % dZ = [qdot, qdotdot, qdotdot], Z = [q, qdot, qdotdot]
