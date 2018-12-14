a003151bb72ca8d756adf9b78c52407afafd4cc5
Michael Kreutz <m.kreutz@tum.de>
2018-12-14 11:51:35 +0100

    loads: clearer naming of forces

--- Matlab_Code/+AMrotorSIM/+Loads/@Unbalance_static/get_loc_load_vec.m
+++ Matlab_Code/+AMrotorSIM/+Loads/@Unbalance_static/get_loc_load_vec.m
@@ -10,19 +10,19 @@ function get_loc_load_vec(obj,time,node,varargin)
     omega = node.qd(6);
     omegadot = node.qdd(6);
     
-    % Anteil aufgrund Zentripetalkraft
-    F_ZP(1) = unwucht * omega^2 * cos(omega*time + phase);
-    F_ZP(2) = unwucht * omega^2 * sin(omega*time + phase);
+    % due to centripetal force
+    FCentripetalForce(1) = unwucht * omega^2 * cos(omega*time + phase);
+    FCentripetalForce(2) = unwucht * omega^2 * sin(omega*time + phase);
     
-    % Anteil aufgrund der Drehbeschleunigung:
-    F_DB(1) = + unwucht * omegadot * sin(omega*time + phase);
-    F_DB(2) = - unwucht * omegadot * cos(omega*time + phase);
+    % due to angular acceleration
+    FAngularAcceleration(1) = + unwucht * omegadot * sin(omega*time + phase);
+    FAngularAcceleration(2) = - unwucht * omegadot * cos(omega*time + phase);
     
-    % Zusammenstellen
-    obj.h(1) = F_ZP(1) + F_DB(1);
-    obj.h(2) = F_ZP(2) + F_DB(2);
+    % Superposition
+    obj.h(1) = FCentripetalForce(1) + FAngularAcceleration(1);
+    obj.h(2) = FCentripetalForce(2) + FAngularAcceleration(2);
     
-    % evtl. weitere Schritte: Drehmoment aufgrund F_DB hinzufuegen
+    % possible next steps: moment because of FAngularAcceleration
     
     
 end
