3b7056dbd6c5aaee6229f2f9242bfdc27b20b0a7
Michael Kreutz <m.kreutz@tum.de>
2019-01-16 14:17:56 +0100

    checked localisation matrix and gyroscopic matrix

--- Matlab_Code/+AMrotorSIM/+Rotor/+FEMRotor/+Element/@TimoshenkoLinearElement/create_ele_loc_matrix.m
+++ Matlab_Code/+AMrotorSIM/+Rotor/+FEMRotor/+Element/@TimoshenkoLinearElement/create_ele_loc_matrix.m
@@ -1,8 +1,7 @@
 function create_ele_loc_matrix(self)
 
         %Vektorversion der Localisierungsmatrix:
-       % Lv0_ele = [3,9,6,12,1,5,7,11,2,4,8,10];
-       Lv0_ele = [5,9,1,10,6,3,7,11,2,12,8,4]; 
+       Lv0_ele = [3,9,6,12,1,5,7,11,2,4,8,10]; %from Genta to global order
        
         %Matrixversion:
         L_ele = sparse(12,12); 
