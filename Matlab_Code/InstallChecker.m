f534b2fc70635393c21fa478b8f9e4c6aaedff30
Michael Kreutz <m.kreutz@tum.de>
2019-02-20 16:07:00 +0100

    refactoring

--- Matlab_Code/InstallChecker.m
+++ Matlab_Code/InstallChecker.m
@@ -3,8 +3,8 @@
 InstDir = fileparts(which(mfilename));
 addpath(InstDir)
 % add abravibe toolbox
+addpath(strcat(InstDir,filesep,'Abravibe_Toolbox'));
 addpath(strcat(InstDir,filesep,'Abravibe_Toolbox',filesep,'abravibe'));
 addpath(strcat(InstDir,filesep,'Abravibe_Toolbox',filesep,'animate'));
-addpath(strcat(InstDir,filesep,'Abravibe_Toolbox',filesep,'ImpactGui'));
 pfad = strcat(fileparts(which(mfilename)),'/pathdef.m');
 savepath pfad
