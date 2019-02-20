9c13da0f8fbb75b0f69d9a0a6fac14a2a47b805b
Michael Kreutz <m.kreutz@tum.de>
2019-02-20 16:07:12 +0100

    added damping to bearings

--- Matlab_Code/+AMrotorSIM/+Bearings/@SimpleBearing/get_loc_damping_matrix.m
+++ Matlab_Code/+AMrotorSIM/+Bearings/@SimpleBearing/get_loc_damping_matrix.m
@@ -2,6 +2,8 @@ function [D] = get_loc_damping_matrix(self,varargin)
     
      D = sparse(6,6);
     % dof-order: ux,uy,uz,psix,psiy,psiz
+    D(1,1)=self.cnfg.damping;
+    D(2,2)=self.cnfg.damping;
  
     self.damping_matrix = D;
 end
