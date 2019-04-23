90b724e34fcdcef6fcdb16203fa894c0a234ee64
Michael Kreutz <m.kreutz@tum.de>
2019-04-23 20:08:02 +0200

    continued removal of expanded state space

--- Matlab_Code/+AMrotorSIM/+Experiments/@Stationaere_Lsg/private/integrate_function_variant.m
+++ Matlab_Code/+AMrotorSIM/+Experiments/@Stationaere_Lsg/private/integrate_function_variant.m
@@ -1,26 +1,16 @@
-function dZ = integrate_function_variant(t,Zexpanded,Omega, rotorsystem, mat)
+function dZ = integrate_function_variant(t,Z,Omega, rotorsystem, mat)
 
 A=mat.A;
 B=mat.B;
 ndof = length(A)/2;
 
-A=[A, zeros(2*ndof,ndof); zeros(ndof,2*ndof), eye(ndof)];
-B=[B, zeros(2*ndof,ndof); zeros(ndof,2*ndof), zeros(ndof)];
-
-Zexpanded(2*ndof)=Omega; % node on the right is driven with constant omega
-
-qdotdot = Zexpanded(ndof*2+1:end);
-
+Z(2*ndof)=Omega; % node on the right is driven with constant omega
 
 %% Loadvector
-h_ges = rotorsystem.compute_system_load_variant(t,Zexpanded(1:2*ndof),qdotdot);
-h_ges = [h_ges; zeros(ndof,1)];
+h_ges = rotorsystem.compute_system_load_variant(t,Z(1:2*ndof));
 
 %% DGL
-dZ = A*Zexpanded+B*h_ges; 
-
-% expanded state-space to maintain acceleration information for next integration step
-% dZ = [qdot, qdotdot, qdotdot], Z = [q, qdot, qdotdot]
+dZ = A*Z+B*h_ges; 
 
 end
 
