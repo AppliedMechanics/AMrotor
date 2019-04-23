5a82a2c607bad76adb5425142bb39dbf9fc9be42
Michael Kreutz <m.kreutz@tum.de>
2019-04-23 20:00:23 +0200

    removed expanded state-space with acceleration for time-integration, because of possible error in the ss

--- Matlab_Code/+AMrotorSIM/@Rotorsystem/compute_system_load_variant.m
+++ Matlab_Code/+AMrotorSIM/@Rotorsystem/compute_system_load_variant.m
@@ -1,6 +1,6 @@
-function [ss_h]= compute_system_load_variant(self,t, Z, qdotdot)
+function [ss_h]= compute_system_load_variant(self,t, Z)
 
-h_loads = self.assemble_system_loads(t,Z,qdotdot);
+h_loads = self.assemble_system_loads(t,Z);
 
 %% Put together
 h = h_loads;
