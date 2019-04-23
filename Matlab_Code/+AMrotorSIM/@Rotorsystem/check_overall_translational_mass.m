9ded43d83057c6ee62f11a1af3bb95a38e5a5cb5
Michael Kreutz <m.kreutz@tum.de>
2019-04-23 11:09:42 +0200

    overall translational mass: include disc mass

--- Matlab_Code/+AMrotorSIM/@Rotorsystem/check_overall_translational_mass.m
+++ Matlab_Code/+AMrotorSIM/@Rotorsystem/check_overall_translational_mass.m
@@ -3,10 +3,35 @@
 
 function overall_mass = check_overall_translational_mass(self,U)
 
+n_nodes=length(self.rotor.mesh.nodes);
+
+M_disc=sparse(6*n_nodes,6*n_nodes);
+K_disc=sparse(6*n_nodes,6*n_nodes);
+G_disc=sparse(6*n_nodes,6*n_nodes);
+
+for disc = self.discs
+    
+    disc.create_ele_loc_matrix;
+    disc.get_loc_gyroscopic_matrix;
+    disc.get_loc_mass_matrix;
+    disc.get_loc_stiffness_matrix;
+    
+    disc_node = self.rotor.find_node_nr(disc.position);
+    L_ele = sparse(6,6*n_nodes);
+    L_ele(1:6,(disc_node-1)*6+1:(disc_node-1)*6+6)=disc.localisation_matrix;
+    
+    M_disc = M_disc+L_ele'*disc.mass_matrix*L_ele;
+    K_disc = K_disc+L_ele'*disc.stiffness_matrix*L_ele;
+    G_disc = G_disc+L_ele'*disc.gyroscopic_matrix*L_ele;
+end
+
+M = self.rotor.matrices.M + M_disc;
+
+
 U_x = U(:,1);
 U_y = U(:,2);
 U_z = U(:,3);
-M = self.rotor.matrices.M;
+
 
 m_x = U_x'*M*U_x;
 m_y = U_y'*M*U_y;
