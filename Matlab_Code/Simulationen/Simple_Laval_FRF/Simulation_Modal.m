f7247a04746a5443dfb4fdf09cf9b62375fc95af
Michael Kreutz <m.kreutz@tum.de>
2019-06-18 11:01:01 +0200

    simple example for FRF calculation from M,D,K

---
+++ Matlab_Code/Simulationen/Simple_Laval_FRF/Simulation_Modal.m
@@ -0,0 +1,81 @@
+%% Simple Laval - Modalanalyse
+
+
+%% Clean up
+close all
+clear all
+% clc
+%% Import
+
+import AMrotorSIM.*
+Janitor = AMrotorTools.PlotJanitor();
+Janitor.setLayout(2,3);
+
+%% Compute Rotor
+
+Config_Sim_Modal
+
+r=Rotorsystem(cnfg,'Laval-System');
+r.assemble; %fuehrt Funktion assemble.m mit Eingabe Objekt r aus Klasse Rotorsystem aus
+r.show; % Funktion AMrotor\Matlab_Code\+AMrotorSIM\+Rotor\+FEMRotor\@FeModel\print.m
+
+
+r.rotor.show_2D(); % compare discretisation and user input
+% r.rotor.geometry.show_2D(); 
+% r.rotor.geometry.show_3D(); % funktioniert nicht richtig
+
+% r.rotor.mesh.show_2D(); 
+% r.rotor.mesh.show_2D_nodes(); 
+%r.rotor.mesh.show_3D();
+
+g=Graphs.Visu_Rotorsystem(r);
+g.show();
+
+
+r.rotor.assemble_fem;
+
+% u_trans_rigid_body = r.compute_translational_rigid_body_modes;overall_mass = r.check_overall_translational_mass(u_trans_rigid_body)
+
+
+%% Running system analyses
+
+%% Berechne die Frequenzgangfunktion
+rpm=1000;
+f=0:2:1000;
+omega=rpm/60*2*pi;
+[M,C,G,K] = r.assemble_system_matrices(rpm);
+D = C+omega*G;
+%Funktion findet die nächstgelegenen Knoten zu den angegebenen inputs und
+%outputs und setzt die Freiheitsgradnumer der dazugehörigen x-Richtung in
+%die Funtion mck2frf von Abravibe ein, 
+% siehe dazu auch die Hilfe von mck2frf
+tic,input=280e-3;output=333e-3;H = get_FRF_from_MDK(r,f,M,D,K,input,output,'d');toc %'d' fuer displacement; 
+figure,subplot(2,1,1),semilogy(f,abs(H),'DisplayName','FEM');ylabel('FRF magnitude'),subplot(2,1,2),plot(f,angle(H)*180/pi,'DisplayName','FEM');xlabel('f//Hz'),ylabel('angle')
+clear M D K C G
+
+
+%% Modalanalyse
+m=Experiments.Modalanalyse(r);
+
+m.calculate_rotorsystem(8,0e3);
+
+esf= Graphs.Eigenschwingformen(m);
+esf.print_frequencies();
+esf.plot_displacements();
+esf.set_plots('half') % 'all', 'half' or desired mode number
+%esf.set_plots('half','overlay')
+% esf.set_plots(10,'Overlay','Skip',5,'tangentialPoints',30,'scale',3) %specify additional options, first input is index of mode
+Janitor.cleanFigures();
+
+
+%% Campbell-Diagramm
+cmp = Experiments.Campbell(r);
+cmp.set_up(1e2:1e2:10e3,8); 
+cmp.calculate();% input of set_up is (1/min, Number of Modes)
+cmpDiagramm = Graphs.Campbell(cmp);
+cmpDiagramm.print_damping_zero_crossing();
+cmpDiagramm.print_critical_speeds()
+cmpDiagramm.set_plots('all');
+% cmpDiagramm.set_plots('backward');
+% cmpDiagramm.set_plots('forward');
+Janitor.cleanFigures();
