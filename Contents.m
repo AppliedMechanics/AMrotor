58fd8c6b917ac03a9dcb7d09014b1061eb4ce261
Michael Kreutz <m.kreutz@tum.de>
2019-06-27 12:25:26 +0200

    Revert "Abravibe as Package wih Matlab-notation (+Abravibe...)"
    This reverts commit 8b627940a39883eeeb79caf9bcbb1424fc857799.

--- Contents.m
+++
@@ -1,7 +0,0 @@
-% AMROTOR
-%
-% Packages
-%   AMrotorSIM - Main simulation Toolbox
-%   AMrotorTools - Tools that the simulation code uses
-%   AMrotorMONI - Monitoring
-%   AbravibeToolbox - Mainly experimental modal analysis
