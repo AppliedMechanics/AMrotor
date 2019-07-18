6ce3deec43ece9512501dbbd59195386acd9239e
Michael Kreutz <m.kreutz@tum.de>
2019-07-18 09:15:29 +0200

    revert changes in compute_ode15s_ss, compute_ode15s_ss_variant, because of mistake in the previous commit the changes are supposed to be applied after the creation of the pidController-class in seperate branch

--- +AMrotorSIM/+Experiments/@Stationaere_Lsg/compute_ode15s_ss.m
+++ +AMrotorSIM/+Experiments/@Stationaere_Lsg/compute_ode15s_ss.m
@@ -1,58 +1,49 @@
-function compute_ode15s_ss_variant(obj)
+     function compute_ode15s_ss(obj)
+    
+        
+        Timer = AMrotorTools.Timer();
+         
+        disp('Compute.... ode15s State Space ....')
+        
+        obj.check_for_non_integrable_components;
+        
+        obj.clear_time_result()
+        
+        obj.result = containers.Map('KeyType','double','ValueType','any');
+        
+       for drehzahl = obj.drehzahlen 
+        disp(['... rotational speed: ',num2str(drehzahl),' U/min'])
+        n_nodes = length(obj.rotorsystem.rotor.mesh.nodes);
+        
+        omega = drehzahl*pi/30;           
+        
+        ss_A = obj.rotorsystem.systemmatrices.ss_A;
 
-Timer = AMrotorTools.Timer();
+        %% Mit null belegen:
+         Z0 = zeros(length(ss_A),1);
+%         for node = 1:n_nodes
+%             dof_psi_z = obj.rotorsystem.rotor.get_gdof('psi_z',node);
+%             Z0(dof_psi_z+size(ss_A,1)/2) = omega; %Alle Positionen der psidot_z Koordinaten mit omega auffüllen
+%         end
+        
+        % solver parameters
+        options = odeset('AbsTol', 1e-5, 'RelTol', 1e-5,'OutputFcn',@odeOutputFcn_plotBeam);
 
-disp('Compute.... ode15s State Space ....')
-obj.rotorsystem.check_for_non_integrable_components;
-obj.clear_time_result()
+        Timer.restart();
+        disp('... integration started...')
+        
+        sol = ode15s(@integrate_function,obj.time,Z0, options, omega, obj.rotorsystem);
+        
+        disp(['... spent time for integration: ',num2str(Timer.getWallTime()),' s'])
 
-obj.result = containers.Map('KeyType','double','ValueType','any');
+        [Z,Zp] = deval(sol,obj.time);
+        
+        res.X = Z(1:6*n_nodes,:);
+        res.X_d = Z(6*n_nodes+1:2*6*n_nodes,:);
+        res.X_dd= Zp(6*n_nodes+1:2*6*n_nodes,:);
+        
+        obj.result(drehzahl)=res;
+        
+       end
 
-
-for rpm = obj.drehzahlen
-    disp(['... rotational speed: ',num2str(rpm),' U/min'])
-    
-    n_nodes = length(obj.rotorsystem.rotor.mesh.nodes);
-    
-    Omega = rpm*pi/30;
-    
-    [mat.A,mat.B] = obj.get_state_space_matrices_variant(Omega);
-    
-    %%init Vector
-    ndof = length(mat.A)/2;
-    Z0 = zeros(2*ndof,1);     % Mit null belegen:
-    Z0(1*ndof+6:6:2*ndof)=Omega;        % Drehzahl fuer psi_z
-    
-    
-    % solver parameters
-    options = odeset('AbsTol', 1e-5, 'RelTol', 1e-5,'OutputFcn',@odeOutputFcn_plotBeam,'MaxStep',obj.time(2)-obj.time(1));
-    %         options = odeset('AbsTol', 1e-5, 'RelTol', 1e-5); % ohne live-plot
-    
-    Timer.restart();
-    disp('... integration started...')
-    
-    
-    sol = ode15s(@integrate_function_variant,obj.time,Z0, options, Omega, obj.rotorsystem, mat);
-    
-    disp(['... spent time for integration: ',num2str(Timer.getWallTime()),' s'])
-    
-    [Z,Zp] = deval(sol,obj.time);
-    
-    res.X = Z(1:6*n_nodes,:);
-    res.X_d = Z(6*n_nodes+1:2*6*n_nodes,:);
-    res.X_dd= Zp(6*n_nodes+1:2*6*n_nodes,:);
-    
-    % Kraftberechnung
-    h_ges = zeros(2*size(res.X,1),size(res.X,2));
-    if any(strcmp({obj.rotorsystem.sensors.type},'ForceLoadPostSensor'))
-        for k = 1:length(obj.time)
-            h_ges(:,k) = obj.rotorsystem.compute_system_load_variant(obj.time(k), [res.X(:,k); res.X_d(:,k)]);
-        end
-        res.F = h_ges(end/2+1:end,:);
-    end
-    
-    obj.result(rpm)=res;
-    
-end
-
-end
+     end
