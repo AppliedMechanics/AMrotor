429612995e59bd382c9032f4f1d74b1638a87f98
Michael Kreutz <m.kreutz@tum.de>
2019-07-23 10:13:49 +0200

    Revert "moved methods for calculaion of forces after the integration to more general @Rotorsystem"
    This reverts commit f9b3dec4ba3cf1e680af1399e475e725942e4f1d.

---
+++ +AMrotorSIM/+Experiments/@Hochlaufanalyse/private/integrate_function_variant.m
@@ -0,0 +1,32 @@
+function dZ = integrate_function_variant(t,Z,rpm_span,t_span, rotorsystem)
+
+rpm_curr = rpm_span(1) + (rpm_span(2)-rpm_span(1)) / (t_span(2)-t_span(1)) * t; % aktuelle Drehzahl
+Omega_curr = rpm_curr/60*2*pi;
+
+[M,C,G,K] = rotorsystem.assemble_system_matrices(rpm_curr); % Systemmatrizen sind in jedem Zeitschritt anders wegen Seal und Bearing rpm-abhaengig
+D = C + Omega_curr*G;
+
+ndof = length(Z)/3;
+Z(2*ndof) = Omega_curr; % node on the right is driven with current oemga
+qdotdot = Z(ndof*2+1:end);
+
+%% Loadvector
+h_ges = rotorsystem.compute_system_load_ss(t,Z(1:2*ndof),qdotdot); 
+
+%% DGL 
+% sollte schneller sein als alte Berechnung mit Vorabberechnung der
+% Systemmatritzen, da sonst in jedem Zeitschritt die volle M-Matrix
+% invertiert werden würde
+Z1 = Z(1 : ndof);
+Z2 = Z(ndof+1: 2*ndof);
+Z3 = Z(2*ndof+1 : 3*ndof);
+dZ1 = Z2;
+dZ2 = M \ (-K*Z1 -D*Z2 +h_ges(end/2+1:end));
+dZ3 = Z3;
+dZ = [dZ1; dZ2; dZ3]; 
+
+% expanded state-space to maintain acceleration information for next integration step
+% dZ = [qdot, qdotdot, qdotdot], Z = [q, qdot, qdotdot]
+
+end
+
--- +AMrotorSIM/+Experiments/@Stationaere_Lsg/calculate_force_load_post_sensor.m
+++ +AMrotorSIM/+Experiments/@Stationaere_Lsg/calculate_force_load_post_sensor.m
@@ -1,14 +1,15 @@
-function F = calculate_force_load_post_sensor(rotorsystem,time,displacement,velocity)
+function F = calculate_force_load_post_sensor(obj,displacement,velocity)
 % calculate the force of the sensor ForceLoadPostSensor
 %   Uses the displacment and velocity to obtain the force of
 %   the corresponding load-objects, through their force laws
 F = zeros(size(displacement,1),size(displacement,2));
 % Kraftberechnung
-if any(strcmp({rotorsystem.sensors.type},'ForceLoadPostSensor'))
+if any(strcmp({obj.rotorsystem.sensors.type},'ForceLoadPostSensor'))
     h_ges = zeros(size(displacement,1),size(displacement,2));
-    for k = 1:length(time)
-        h_ges(:,k) = rotorsystem.assemble_system_loads(time(k), [displacement(:,k); velocity(:,k)]);
+    for k = 1:length(obj.time)
+        h_ges(:,k) = obj.rotorsystem.assemble_system_loads(obj.time(k), [displacement(:,k); velocity(:,k)]);
     end
     F = h_ges;
 end
+
 end
