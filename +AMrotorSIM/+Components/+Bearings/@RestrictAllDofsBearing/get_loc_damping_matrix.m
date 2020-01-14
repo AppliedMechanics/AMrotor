3a85df88d1465a3b85689127ad53a5ec9f31dc3a
Michael Kreutz <m.kreutz@tum.de>
2020-01-14 10:27:58 +0100

    corrected small error

--- +AMrotorSIM/+Components/+Bearings/@RestrictAllDofsBearing/get_loc_damping_matrix.m
+++ +AMrotorSIM/+Components/+Bearings/@RestrictAllDofsBearing/get_loc_damping_matrix.m
@@ -3,7 +3,7 @@ function [D] = get_loc_damping_matrix(self,varargin)
      D = sparse(6,6);
      % dof-order: ux,uy,uz,psix,psiy,psiz
      for i = 1:6
-         D(i,i)=self.cnfg.stiffness;
+         D(i,i)=self.cnfg.damping;
      end
     self.damping_matrix = D;
 end
