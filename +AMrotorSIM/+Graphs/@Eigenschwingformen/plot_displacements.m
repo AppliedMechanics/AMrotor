740e3ac84203efef3bf22da850162d343d33fffc
Michael Kreutz <m.kreutz@tum.de>
2020-02-19 08:47:01 +0100

    Modalanalyse: plot displacements code more compact and easier to read

--- +AMrotorSIM/+Graphs/@Eigenschwingformen/plot_displacements.m
+++ +AMrotorSIM/+Graphs/@Eigenschwingformen/plot_displacements.m
@@ -5,87 +5,23 @@ function [x,EVmain] = plot_displacements(obj)
   ColorHandler = AMrotorTools.PlotColors();
   ColorHandler.set_up(n_ew);
 
-  % plotten der Moden
-  figure('Name','Eigenschwingformen x','NumberTitle','off');
-  ax = axes;
-    title('Eigenmoden Lateral-Richtung x')
-    hold on;
-
+    % 
+    EW = imag(obj.modalsystem.eigenValues.lateral);
     x = [obj.modalsystem.rotorsystem.rotor.mesh.nodes.z];
     EVx = obj.modalsystem.eigenVectors.lateral_x;
-    EW = imag(obj.modalsystem.eigenValues.lateral);
     
-    for s=1:n_ew
-        plotMode(ax,x,EVx(:,s),...
-                       EW(s),...
-                       ColorHandler.getColor(s))
-    end
-
-    legend('show')
-    grid('on')
+    % write the data that should be plotted in CellArray
+    stringCellArray = {'u_x'};
+    EVCellArray = {EVx};
 
     if isfield(obj.modalsystem.eigenVectors,'lateral_y')
         EVy = obj.modalsystem.eigenVectors.lateral_y;
         EVz = obj.modalsystem.eigenVectors.lateral_z;
         EVpsiz = obj.modalsystem.eigenVectors.torsional_psi_z;
-    
-        figure('Name','Eigenschwingformen y','NumberTitle','off');
-        ax = axes;
-        title('Eigenmoden Lateral-Richtung y')
-        hold on;
 
-        for s=1:n_ew
-            plotMode(ax,x,EVy(:,s),EW(s),ColorHandler.getColor(s))
-        end
+        EVr = sqrt(EVx.^2+EVy.^2);
 
-        legend('show')
-        grid('on')
-        
-        
-        figure('Name','Eigenschwingformen z','NumberTitle','off');
-        ax = axes;
-        title('Eigenmoden Lateral-Richtung z')
-        hold on;
-
-        for s=1:n_ew
-            plotMode(ax,x,EVz(:,s),EW(s),ColorHandler.getColor(s))
-        end
-
-        legend('show')
-        grid('on')
-        
-        
-        figure('Name','Eigenschwingformen psi z','NumberTitle','off');
-        ax = axes;
-        title('Eigenmoden Torsions-Richtung psi z')
-        hold on;
-
-        for s=1:n_ew
-            plotMode(ax,x,EVpsiz(:,s),EW(s),ColorHandler.getColor(s))
-        end
-
-        legend('show')
-        grid('on')
-        
-        
-        figure('Name','Eigenschwingformen sqrt(x^2+y^2)','NumberTitle','off');
-        ax = axes;
-        title('Eigenschwingformen r=sqrt(x^2+y^2)')
-        hold on;
-
-        for s=1:n_ew
-            plotMode(ax,x,sqrt(EVx(:,s).^2+EVy(:,s).^2),EW(s),...
-                ColorHandler.getColor(s))
-        end
-
-        legend('show')
-        grid('on')
-        
-        figure('Name','Eigenschwingformen in Hauptachsen','NumberTitle','off');
-        ax = axes;
-        title('Eigenschwingformen in Hauptachsen')
-        hold on;
-
+        % rotate the coordinate system for each EV to see the biggest amplitude
         EVmain = zeros(size(EVx));
         for s=1:n_ew
             angle = atan2(EVy(:,s),EVx(:,s));
@@ -97,14 +33,25 @@ function [x,EVmain] = plot_displacements(obj)
                 end
             end
             angle = mean(angle);
-            
             EVmain(:,s) = EVx(:,s)*cos(angle) + EVy(:,s)*sin(angle);
-            plotMode(ax,x,EVmain(:,s),EW(s),...
-                ColorHandler.getColor(s))
         end
 
-        legend('show')
-        grid('on')
+        stringCellArray = {stringCellArray{1:end},'u_y','u_z','psi_z','r=sqrt(u_x^2+u_y^2)','main axes'}; %just append
+        EVCellArray = {EVCellArray{1:end}, EVy, EVz, EVpsiz, EVr, EVmain};
+    end
+
+
+    % plot the data in the CellArrays
+    for iCell = 1:length(EVCellArray)
+            figure('Name',['eigenmodes ',stringCellArray{iCell}],'NumberTitle','off');
+            ax = axes;
+            title(['eigenmodes ',stringCellArray{iCell}])
+            hold on;grid('on');
+            for s=1:n_ew
+                plotMode(ax,x,EVCellArray{iCell}(:,s),EW(s),ColorHandler.getColor(s))
+            end
+            legend('show')
+    end
     end
     
 end
