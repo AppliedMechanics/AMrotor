b8b481355e11bfd2e9369a7b96a9d899e1e2fc48
Michael Kreutz <m.kreutz@tum.de>
2020-03-19 17:08:18 +0100

    fix: obsolete Matlab-toolbox-command

--- +AMrotorSIM/+Experiments/@Modalanalyse/do_mass_normalization.m
+++ +AMrotorSIM/+Experiments/@Modalanalyse/do_mass_normalization.m
@@ -1,6 +1,6 @@
 function VMassNormalized = do_mass_normalization(obj,V,M)
-% perform mass normalization of the modes, so that transp(V)*M*V=1
-modalMasses = diag(transp(V)*M*V);
+% perform mass normalization of the modes, so that transpose(V)*M*V=1
+modalMasses = diag(transpose(V)*M*V);
 tmp = 1./sqrt(modalMasses);
 VMassNormalized = V*diag(tmp);
 end
