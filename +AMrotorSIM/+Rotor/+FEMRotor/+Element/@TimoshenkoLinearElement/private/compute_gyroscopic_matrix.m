299ba9c595012844a3dd1d77162ede04a2bc27ca
Johannes Maierhofer <j.maierhofer@tum.de>
2020-03-27 14:48:12 +0100

    Bug-fixes

--- +AMrotorSIM/+Rotor/+FEMRotor/+Element/@TimoshenkoLinearElement/private/compute_gyroscopic_matrix.m
+++ +AMrotorSIM/+Rotor/+FEMRotor/+Element/@TimoshenkoLinearElement/private/compute_gyroscopic_matrix.m
@@ -110,5 +110,5 @@ function [G_ele] = compute_gyroscopic_matrix(Element)
     G2 = a*G2;
     
     %Gyroscopic Matrix for Element
-    G_ele = -(G0+phi*G1+phi^2*G2); %Eintraege vertauscht, damit forward und backward whirl stimmen
+    G_ele = -(G0+phi*G1+phi^2*G2);
 end
