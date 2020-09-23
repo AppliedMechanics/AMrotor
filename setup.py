672556ef85bdeaf1e0ec948473a11cec5333c870
Tobias Mulser <tobias.mulser@gmail.com>
2020-09-23 21:54:52 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(
-   name='AMrotor',
+   name='AMrotor_doc',
    version='1.0',
    description='Matlab based rotordynamic toolbox',
    author='AMrotorTeam',
    author_email='foomail@foo.com',
-   packages=['AMrotor'],  #same as name
+   packages=['AMrotor_doc'],  #same as name
    install_requires=['bar', 'greek'], #external packages as dependencies
 )
