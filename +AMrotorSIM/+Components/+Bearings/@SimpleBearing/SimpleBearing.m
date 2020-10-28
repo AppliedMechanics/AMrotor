1cc9b491068efe9e847f67ec118bfa72848ce9d4
Michael Kreutz <m.kreutz@tum.de>
2020-10-28 13:15:58 +0100

    tiny correction in comment

--- +AMrotorSIM/+Components/+Bearings/@SimpleBearing/SimpleBearing.m
+++ +AMrotorSIM/+Components/+Bearings/@SimpleBearing/SimpleBearing.m
@@ -1,5 +1,5 @@
 classdef SimpleBearing < AMrotorSIM.Components.Bearings.Bearing
-% SimpleAxialBearing acts on the x and y direction of the corresponding node
+% SimpleBearing acts on the x and y direction of the corresponding node
 %   Add stiffness and damping on the x and y degrees of freedom of the
 %   closest node of the rotor 
    properties
